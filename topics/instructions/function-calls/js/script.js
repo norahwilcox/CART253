1a8b4b2e3c7195b641f0a75039a5ab89a1bbe112
norahwilcox <norah@wilcoxfamily.ca>
2025-09-09 15:34:36 -0400

    Implemented the Greatest Record of all time

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,23 +1,45 @@
 /**
- * Drawing Module 
+ * The Greatest Record of all Time 
  * Norah Wilcox
  * 
- * Practicing and learning drawing functions in p5
+ * Displays the createst record of all time
  */
 
 "use strict";
 
 /**
- * DIDN'T DESCRIBE SETUP
+ * Creates square canvas
  */
 
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 /**
- * DIDN'T DESCRIBE DRAW
+ * Displays the background 
  */
 function draw() {
+    // Grey background 
+    background(150);
 
+    // The main part of the record 
+    push();
+    fill(255, 0, 0);
+    stroke(255);
+    ellipse(320, 320, 480);
+    pop();
+
+    // the label of the rcord 
+    push();
+    fill('white');
+    noStroke();
+    ellipse(320, 320, 140);
+    pop();
+
+    // The hole in the record 
+    push();
+    fill("#000000");
+    noStroke();
+    ellipse(320, 320, 20);
+    pop();
 }
