1eade47bb51eb8c1f9249fd8b2b75d95bfd4173e
norahwilcox <norah@wilcoxfamily.ca>
2025-09-09 15:05:26 -0400

    ready for drawing module

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,32 +1,23 @@
 /**
- * The Blank Page
+ * Drawing Module 
  * Norah Wilcox
  * 
- * An exploration of the existential angst of a novelist
- * who must sit down at their pink desk and confront the 
- * abyss that is a page of paper.
- * 
- * The program is non-ingteractive to convey the inability 
- * to get started on the project. Try typing! Just try it!
+ * Practicing and learning drawing functions in p5
  */
 
 "use strict";
 
 /**
- * Creates the canvas for our masterpiece
+ * DIDN'T DESCRIBE SETUP
  */
 
 function setup() {
-    // Create the canvas at a standard resolution
-    createCanvas(640, 480);
+
 }
 
 /**
- * Draws the writer's desktop and a blank piece of paper
+ * DIDN'T DESCRIBE DRAW
  */
 function draw() {
-    // The pink desktop
-    background(255, 100, 100);
-    // The piece of paper
-    rect(200, 80, 240, 320);
+
 }
