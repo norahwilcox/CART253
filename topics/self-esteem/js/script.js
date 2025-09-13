cb3f5367999c02737cc5899fd4631bcada8fd93e
norahwilcox <norah@wilcoxfamily.ca>
2025-09-13 16:29:43 -0400

    Upgraded Self-esteem project

--- topics/self-esteem/js/script.js
+++ topics/self-esteem/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Self-Esteem
- * Pippin Barr
+ * Pippin Barr and Norah Wilcox
  * 
  * A portrait of Pippin's self-esteem on a sunny day.
  */
@@ -13,15 +13,14 @@ let skyGreen = 180;
 let skyBlue = 250;
 
 // The sun
-
-// Colour
-let sunRed = 255;
-let sunGreen = 255;
-let sunBlue = 0;
-// Position and size
-let sunX = 500;
-let sunY = 70;
-let sunSize = 100;
+let sun = {
+    red: 255,
+    green: 255,
+    blue: 0,
+    x: 500,
+    y: 70,
+    size: 100
+};
 
 // My self-esteem
 
@@ -49,9 +48,9 @@ function draw() {
 
     // The sun
     push();
-    fill(sunRed, sunGreen, sunBlue);
+    fill(sun.red, sun.green, sun.blue);
     noStroke();
-    ellipse(sunX, sunY, sunSize);
+    ellipse(sun.x, sun.y, sun.size);
     pop();
 
     // My self esteem
