81758ceeae658bd82c387f7c165c2cd10ad2b5ab
norahwilcox <norah@wilcoxfamily.ca>
2025-09-29 16:23:45 -0400

    started debugging events

--- topics/events-week5/template-p5-project/js/script.js
+++ topics/events-week5/template-p5-project/js/script.js
@@ -1,24 +1,48 @@
 /**
- * 
+ * Events in Plain Java Script
  * Norah Wilcox
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Experimenting with event handling
  */
 
 "use strict";
 
+// Info about the possible background fills
+const bg = {
+    fill: "#000000",
+    fills: {
+        black: "000000",
+        white: "#ffffff"
+    }
+}
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
+    createCanvas(400, 400);
 
+    window.addEventListener("keydown", changeBG);
 }
-
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Displays the background
 */
 function draw() {
+    background(bg.fill);
+}
+
+function changeBG(event) {
+    if (event.keyCode === bg.switchKey) {}
+    if (bg.fill === bg.fills.black) {
+        bg.fill = bg.fills.white;
+    }
+    else {
+        bg.fill = bg.fills.black;
+    }
+}
+
+function keyPressed() {
 
 }
+
