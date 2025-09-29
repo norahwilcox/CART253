32dd602fe3ba13b005b50a82175a30603d0ea071
norahwilcox <norah@wilcoxfamily.ca>
2025-09-29 16:40:59 -0400

    finished events learning materials

--- topics/events-week5/debugging-events/js/script.js
+++ topics/events-week5/debugging-events/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Debugging Events
- * Pippin Barr
+ * Norah Wilcox
  * 
  * Two player game. The bug tries to grow as big as possible by
  * running around with the arrow keys. The other player tries to
@@ -30,7 +30,7 @@ const bug = {
 };
 
 // The game is 15 seconds long
-let gameDuration = 15;
+let gameDuration = 15 * 1000;
 // Is the game over?
 let gameOver = false;
 
@@ -41,10 +41,10 @@ function setup() {
     createCanvas(500, 500);
 
     // Listen for keyboard presses for the controls
-    document.addEventListener("keypress", controlsHandler);
+    document.addEventListener("keydown", controlsHandler);
 
     // End the game after its duration
-    setTime(endTheGame, gameDuration);
+    setTimeout(endTheGame, gameDuration);
 }
 
 /**
@@ -98,8 +98,8 @@ function drawBug() {
 /**
  * Sets the game to be over
  */
-function endGame() {
-    gameOver === true;
+function endTheGame() {
+    gameOver = true;
 }
 
 /**
@@ -119,7 +119,7 @@ function drawGameOver() {
 /**
  * Arrow keys increase bug velocity in that direction (infinitely)
  */
-function controlsHandler() {
+function controlsHandler(event) {
     if (event.keyCode === LEFT_ARROW) {
         bug.velocity.x += -bug.speedUp;
     }
@@ -135,9 +135,9 @@ function controlsHandler() {
 }
 
 /**
- * If the bug gets clicked it dies
+ * If the bug gets clicked it shinks
  */
-function mouseIsPressed() {
+function mousePressed() {
     // Don't check clicks when the game is over
     if (gameOver) {
         return;
