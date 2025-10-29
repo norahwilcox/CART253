20d235dbc0672af07fb10bb786c9e8b7f79a5f21
norahwilcox <norah@wilcoxfamily.ca>
2025-10-29 19:34:19 -0400

    finishing in class arrays

--- topics/arrays-week8/template-p5-project/js/script.js
+++ topics/arrays-week8/template-p5-project/js/script.js
@@ -8,11 +8,60 @@
 
 "use strict";
 
+
+function createNewBall() {
+    let ball = {
+        x: 0,
+        y: 0,
+        size: 0,
+        color: {
+            r: 0,
+            g: 0,
+            b: 0,
+        }
+    };
+    return ball;
+}
+
+let numbers = [];
+numbers.push(2);
+console.log(numbers);
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    let balls = [ {
+        x: random(0, 100),
+        y: random(0, 100),
+        size: random(0, 100),
+        color: {
+            r: random(0, 255),
+            g: (0, 255),
+            b: (0, 255),
+        }
+}, 
+    {
+        x: random(0, 100),
+        y: random(0, 100),
+        size: random(0, 100),
+        color: {
+            r: random(0, 255),
+            g: (0, 255),
+            b: (0, 255),
+        }
+},
+     {
+        x: 0,
+        y: 0,
+        size: 0,
+        color: {
+            r: 0,
+            g: 0,
+            b: 0,
+        }
+    }, 
+];
 }
 
 
@@ -20,5 +69,12 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+
+}
+
+function mousePressed() {
+    numbers.push(random(0, 1000));
+    console.log(numbers[numbers.length])
+
 
 }
