a362e793115eff9cbaf0d190aae7eeb10b1fb215
norahwilcox <norah@wilcoxfamily.ca>
2025-11-04 15:04:59 -0500

    deleted not used element, commented the work and filled in the Reed Me

--- topics/mod-jam/README.md
+++ topics/mod-jam/README.md
@@ -1,35 +1,35 @@
-# TITLE OF PROJECT
+# You Look Fly!
 
-AUTHOR NAME
+Norah Wilcox
 
-[View this project online](URL_FOR_THE_RUNNING_PROJECT)
+[View this project online](https://norahwilcox.github.io/CART253/topics/mod-jam/)
 
 ## Description
 
-This description should help the reader understand what the program is, anything they should know to be able to experience it (controls, special features, etc.), and what the desired user experience is. For example:
+> "You Look Fly!" is a cute point and click game that allow the player to catch flies as a little frog sitting on a rock.
 
-> *The Mayor of Clown Town* is a simulator experience that allows the user to control a small town populated entirely by clowns.
+> The experience is controlled via the mouse, with left click controlling the frog's tongue movement.
 
-> The experience is controlled via the mouse, with left click selecting a clown and bringing up a menu of options such as "slip of banana peel" or "get into impossible capacious clown-car."
-
-> The project is meant to give the user a sense of what it would be the mayor of a town of clowns, eventually getting the sense that clowns are not taking their civic duties seriously.
+> The game is timed on the left side of the screen. When the timer runs out, the player loses. To win, the player must catch enough flies using the frog's tongue to fill up the counter on the right side of the screen.
 
 ## Screenshot(s)
 
-This bit should have some images of the program running so that the reader has a sense of what it looks like. For example:
+> ![Image of the file background](./assets/images/frog-file-background.png)
 
-> ![Image of a clown face](./assets/images/clown.png)
+> ![Images of the frog's closed and open mouth animation frames](./assets/images/frog-mouth-closed.png)(./assets/images/frog-mouth-open.png)
+
+> ![Image of the counter](./assets/images/frog-counter.png)
+
+> ![Image of the start screen title and instructions](./assets/images/title-1.png)
 
 ## Attribution
 
-This bit should attribute any code, assets or other elements used taken from other sources. For example:
-
 > - This project uses [p5.js](https://p5js.org).
-> - The clown image is a capture of the clown from the Apple emoji character set.
-> - The barking sound effect is "single dog bark 1" by crazymonke9 from freesound.org: https://freesound.org/people/crazymonke9/sounds/418107/
 
-## License
+> - This project's pixel art graphics were created using Aseprite (https://www.aseprite.org).
 
-This bit should include the license you want to apply to your work. For example:
+> - The "miss" sound effect is from Flipnote Studio on DS / DSI found here: https://sounds.spriters-resource.com/ds_dsi/flipnotestudio/asset/422126/ named "Sample 1.wav."
 
-> This project is licensed under a Creative Commons Attribution ([CC BY 4.0](https://creativecommons.org/licenses/by/4.0/deed.en)) license with the exception of libraries and other components with their own licenses.
+> - The "yom" sound effect is from Animal Crossing: Pocket Camp on mobile found here: https://sounds.spriters-resource.com/mobile/animalcrossingpocketcamp/asset/410558/ named "Gal_404-4.mk.16.cn4.wav."
+
+> - The music used is from Wii Play Motion 2011 found here: https://downloads.khinsider.com/game-soundtracks/album/wii-play-motion-wii named " Staff Credits."
--- topics/mod-jam/js/script.js
+++ topics/mod-jam/js/script.js
@@ -1,14 +1,19 @@
 /**
- * Mod Jam
+ * Mod Jam: "You Look Fly!"
  * Norah Wilcox
  * 
+ * A game about catching flies as a little frog on a rock!
  * 
+ * Use mouse-clicks to try and catch the flies and fill the counter on the right 
+ * before the timer on the left runs out. 
  */
 
 "use strict";
 
+// Creates variable for which screen will display to players
 var whichscreen = "start"
 
+// Delares imported images of the backdrop, frog and counters
 let file;
 let pond;
 let frogMouthClosed;
@@ -16,27 +21,39 @@ let frogMouthOpen;
 let counter;
 let timer;
 
+// Declares imported images of the start, success and failure screens 
 let title1;
 let title2;
 let title3;
 
-const fly = {
-    x: 150,
-    y: 150,
-    size: 8,
-};
-
+// Declares all sound effects 
 let yom;
 let miss;
 let success;
 let music;
 let failure;
 
+// Creates reference values for the fly
+const fly = {
+    x: 150,
+    y: 150,
+    size: 8,
+};
+
+// Creates reference values for the frog's tounge
+const tongue = {
+    x: 440,
+    y: 280,
+    size: 8,
+};
+
+// Declares the return point for the frog's tounge
 let x = 425; 
 let y = 248; 
 let targetX = 425;
 let targetY = 248;
 
+// Creates reference values for the shrinking timer
 const fillTimer = {
     x: 75,
     y: 600,
@@ -44,6 +61,7 @@ const fillTimer = {
     h: -500,
 };
 
+// Creates reference values for the rising counter
 const risingCounter = {
     x: 690,
     y: 600,
@@ -51,14 +69,11 @@ const risingCounter = {
     h: -2,
 };
 
-const tongue = {
-    x: 440,
-    y: 280,
-    size: 8,
-};
-
-// Preloads the backdrop and frog 
+/** 
+ * Preloads all image and sound assets before frame 1
+ */
 function preload() {
+    // loads all image assets 
     file = loadImage('/assets/images/frog-file-background.png');
     pond = loadImage('/assets/images/frog-pond-setting.png');
     frogMouthClosed = loadImage('/assets/images/frog-mouth-closed.png');
@@ -70,139 +85,203 @@ function preload() {
     title2 = loadImage('/assets/images/title-2.png');
     title3 = loadImage('/assets/images/title-3.png');
     
+    // loads all audio assets 
     yom = new Audio("./assets/sounds/yom.wav");
     miss = new Audio("./assets/sounds/miss.wav");
-    success = new Audio ("./assets/sounds/success.wav");
     music = new Audio("./assets/sounds/music.wav");
-    failure = new Audio ("./assets/sounds/failure.wav");
 };
 
-// Creates the canvas
+/** 
+ * Creates the canvas
+ */ 
 function setup() {
     createCanvas(800, 700);
-}
+};
 
-// Draws the backdrop and frog, animates the frog eating with mouse click and moves the tongue
+/** 
+ * Draws each screen seen by the player
+ */
 function draw() {
+    // When the page loads, the start screen appears
     if (whichscreen === "start") {
         startScreen()
     }
+    // When the player clicks the mouse, the game screen appears
     else if (whichscreen === "game") {
         gameScreen()
     }
+    // When the player wins the game, the success screen appears
     else if (whichscreen === "success") {
         successScreen()
     }
+    // When the player loses the game, the failure screen appears
     else {
         failureScreen()
     }
-    debug();
 };
 
+/** 
+ * Draws/ Assembles the start screen
+ */ 
 function startScreen() {
     background(255);
+    // Draws the start screen image 
     image(title1, 160, 100, 0, 0);
 
+    // If the mouse is pressed, the game screen is called and the music begins to play
     if (mouseIsPressed) {
         whichscreen = "game"
         music.play();
     }
-    image(file, 0, 0, 0, 0);
-}
 
+    // Draws the file background 
+    image(file, 0, 0, 0, 0);
+};
+
+/** 
+ * Draws/ Assembles the game screen
+ */
 function gameScreen() {
     background(255);
+
+    // Draws the pond image 
     image(pond, 160, 100, 0, 0);
     
+    // Draws the fly and frog and checks when the tongue overlaps with the fly
     createFly();
     animateFrog();
     checkFlyOverlap();
 
+    // If the player fills the counter before the timer runs out, they win!
     if (risingCounter.h <= -500) {
         whichscreen = "success"
     }
+    // If the player's timer runs out before the counter is full, they lose
     else if (fillTimer.h >= 1) {
         whichscreen = "failure"
     }
   
+    // Draws the file background in front of the game elements to keep them contained
     image(file, 0, 0, 0, 0);
 
+    // Draws the counter and timer in front of the file image
     createCounter();
     createTimer();
-}
+};
 
+/** 
+ * Draws/ Assembles the success screen
+ */
 function successScreen() {
+    // Draws the success screen image 
     image(title2, 160, 100, 0, 0);
-    //success.play();
+   
+    // Pauses the music when the player wins
     music.pause();
+
+    // Draws the file background 
     image(file, 0, 0, 0, 0);
 };
 
+/** 
+ * Draws/ Assembles the failure screen 
+ */
 function failureScreen() {
+    // Draws the failure screen image 
     image(title3, 160, 100, 0, 0);
 
-    //failure.play();
+    // Pauses the music when the player fails 
     music.pause();
+
+    // Draws the file background 
     image(file, 0, 0, 0, 0);
 }; 
 
+/**
+ * Draws the timer image and animation
+ */
 function createTimer() {
     push();
     noStroke();
-    fill(161, 190, 96);
+    fill(247, 178, 163);
+    // Draws a rectangle to go inside the timer image which starts filled
     rect(fillTimer.x, fillTimer.y, fillTimer.w, fillTimer.h);
+    // The rectangle's height is consistenly lowered to represent shortening time
     fillTimer.h += 0.2
     pop();
 
+    // Draws the timer image over the timer rectangle 
     image(timer, 63, 80, 0, 0);
 };
 
+/**
+ * Draws the counter image and animation
+ */
 function createCounter() {
     push();
     noStroke();
     fill(161, 190, 96);
+    // Creates a rectangle to go inside the counter image which starts empty
+    // The rectangle's height increases when the player catches a fly
     rect(risingCounter.x, risingCounter.y, risingCounter.w, risingCounter.h);
     pop();
 
+    // Draws the counter image over the counter rectangle 
     image(counter, 675, 80, 0, 0);
 };
 
-// Creates frog animation and sound when mouse is pressed
+/**
+ * Creates the frog + tongue animation and sound when mouse is pressed
+ */
 function animateFrog() {
+    // Draws the frog's closed mouth frame underneath 
     image(frogMouthClosed, 410, 261, 0, 0);
+
+    // Only when the mouse is clicked...
     if (mouseIsPressed) {
+        // the frog's open mouth frame is drawn,
         image(frogMouthOpen, 410, 253, 0, 0);
+        // the miss sound effect is played 
         miss.play();
+        // and the tongue and the tounge's animation is drawn
         drawTongue();
         moveTongue();
     }
 };
 
-// Draws and sets the fly's position
+/**
+ * Creates and sets the fly's position
+ */
 function createFly() {
     push();
     noStroke();
     fill(0);
+    // Draws the fly's inicial poition and size
     rect(fly.x, fly.y, fly.size, fly.size);
     pop();
-    // Resests the fly when it flies off canvas
+
+    // Resests the fly when it goes off the canvas
     if (fly.x > 700 || fly.y > 650) {
         fly.x = 150
         fly.y = 150
     }
-        // Creates flying animation!
+    // Creates the flying animation!
     else {
         fly.x += 3
         fly.y += random(4, -4)
     }
 };
 
+/**
+ * Checks if the fly is caught by the tongue and raises the counter 
+ */
 function checkFlyOverlap() {
     // Get distance from tongue to fly
     const d = dist(x, y, fly.x, fly.y);
+
     // Check if it's an overlap
     if (d < tongue.size / 2 + fly.size / 2) {
-        // Success! Resets the fly and plays gom sound
+        // Success! Resets the fly and plays yom sound
         fly.x = 150
         fly.y = 150
         yom.play();
@@ -210,21 +289,28 @@ function checkFlyOverlap() {
     }
 };
 
-// Gives the tongue delayed movement towards the target area
+/**
+ * Gives tongue delayed movement towards the target area
+ */
 function moveTongue() {
+    // Moves x and y toward the target
     x = lerp(x, targetX, 0.1);
     y = lerp(y, targetY, 0.1);
 };
 
-// Sets x and y when the user clicks the mouse 
+/**
+ * Sets x and y when the user clicks the mouse 
+ */
 function mouseClicked() {
     x = mouseX
     y = mouseY
 };
 
-// Draws the tongue's tip and shape
+/**
+ *  Draws the tongue
+ */ 
 function drawTongue() {
-    // Draws the tongue tip
+    // Draws the tongue's tip
     push();
     fill("#94502b");
     noStroke();
@@ -237,14 +323,4 @@ function drawTongue() {
     strokeWeight(tongue.size);
     line(x, y, tongue.x, tongue.y);
     pop();
-};
-
-/** 
-function handleEnd() {
-  isPlaying = false;
-  isDone = true;
-};
-*/
-function debug(drawTongue) {
-    console.log(image(frogMouthClosed));
 };
