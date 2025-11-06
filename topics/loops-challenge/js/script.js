42aa1b21eefece985c26b212c3f72986954e88c2
norahwilcox <norah@wilcoxfamily.ca>
2025-11-05 20:08:56 -0500

    starting loops challenge

--- topics/loops-challenge/js/script.js
+++ topics/loops-challenge/js/script.js
@@ -1,24 +1,52 @@
 /**
- * Title of Project
- * Author Name
+ * Lines
+ * Pippin Barr
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A series of lines across the canvas
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Creates the canvas
+ */
 function setup() {
-
+    createCanvas(500, 500);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draws lines across the canvas with increasing thickness and
+ * gradually lightening colour
+ */
 function draw() {
+    background("pink");
+
+    let x;
+    let y = 25;
+
+        // Draw vertical lines using a loop.
+        for (let x = 0; x < 500; x += 1.5) {
+            stroke(0 + y)
+            line(x, 1, x, 500);
+    
+            // Increment by 20.
+            y += 0.5;
+        }
+    
+    x = 50;
+    y = 25;
+
+  // Repeat as long as x < 100
+    while (x < 500) {
+        stroke(0 + y)
+        line(x, 0, x, 500);
+
+        stroke(0 + y)
+        line(0, x, 500, x);
+
+        // Increment by 20.
+        x += 50;
+        y += 25;
+    }
 
 }
