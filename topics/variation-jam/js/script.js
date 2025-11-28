cedc2ad50e5f0b297d67c5a6b9de3fa5dc8d0b06
norahwilcox <norah@wilcoxfamily.ca>
2025-11-28 00:16:01 -0500

    Added all the character assets, made the start screen mouse clickable and created the file where the selection will be

--- topics/variation-jam/js/script.js
+++ topics/variation-jam/js/script.js
@@ -9,23 +9,45 @@
 "use strict";
 
 var whichscreen = "start"
+var whichfile = "head"
 
-let button1;
+let start;
 let startButtonUp;
 let startButtonDown;
 let title;
 let namez;
+let file;
+let openTab;
+let closedTab;
+
+/** 
+let faceText;
+let hairText;
+let endText;
+let headText;
+*/
 
 function preload() {
-    startButtonUp = loadImage('./assets/images/Start_button_up.png');
-    startButtonDown = loadImage('./assets/images/Start_button_down.png');
-    title = loadImage('./assets/images/Title_screen.png');
-    namez = loadImage('./assets/images/name.png');
+  startButtonUp = loadImage('./assets/images/Start_button_up.png');
+  startButtonDown = loadImage('./assets/images/Start_button_down.png');
+  title = loadImage('./assets/images/Title_screen.png');
+  namez = loadImage('./assets/images/name.png');
+  
+  file = loadImage('./assets/images/file.png');
+  openTab = loadImage('./assets/images/file.tab.light.png');
+  closedTab = loadImage('./assets/images/file.tab.dark.png');
+
+  /** 
+  faceText = loadImage('./assets/images/Face.png');
+  headText = loadImage('./assets/images/Head.png');
+  hairText = loadImage('./assets/images/Hair.png');
+  endText = loadImage('./assets/images/End.png');
+  */
 }
 
 function setup() {
   createCanvas(1200, 800);
-  button1 = new Button(495, 520, startButtonUp);
+  start = new Start(495, 520, startButtonUp);
 }
 
 function draw() {
@@ -44,21 +66,73 @@ function draw() {
         productScreen()
     }
   
-  
 }
 
 function startScreen() {
-    image(title, 370, 200);
-    image(namez, 460, 310);
-  button1.display();
+  image(title, 370, 200);
+  image(namez, 460, 310);
+  start.display();
 }
 
 function gameScreen() {
-  
+  if (whichfile === "head") {
+        headFile()
+    }
+    else if (whichfile === "face") {
+        faceFile()
+    }
+    else if (whichfile === "hair") {
+        hairFile()
+    }
+    else {
+        endFile()
+  }
+
+  image(file, 430, 150);
 }
 
-class Button {
-  
+function headFile() {
+  image(openTab, 460, 110);
+
+  image(closedTab, 617, 110);
+
+  image(closedTab, 774, 110);
+
+  image(closedTab, 931, 110);
+}
+
+function faceFile() {
+  image(closedTab, 460, 110);
+
+  image(openTab, 617, 110);
+
+  image(closedTab, 774, 110);
+
+  image(closedTab, 931, 110);
+}
+
+function hairFile() {
+  image(closedTab, 460, 110);
+
+  image(closedTab, 617, 110);
+
+  image(openTab, 774, 110);
+
+  image(closedTab, 931, 110);
+}
+
+function endFile() {
+  image(closedTab, 460, 110);
+
+  image(closedTab, 617, 110);
+
+  image(closedTab, 774, 110);
+
+  image(openTab, 931, 110);
+}
+
+
+class Start {
   constructor(inX, inY, inImg) {
     this.x = inX;
     this.y = inY;
@@ -84,5 +158,14 @@ class Button {
     } else {
       return false;
     }
+  }
+}
+
+function mousePressed() {
+    if (whichscreen === "start" && start.over()) {
+        whichscreen = "game";
+    }
+    else if (whichscreen === "game" && button2.over()) {
+      
   }
 }
